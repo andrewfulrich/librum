09530a4d09473cc9312cfd6da9681e72688e7b4a
Nehemiah I. Dacres <ndacres@gainsight.com>
2015-10-11 08:50:58 -0500

    added button to payment_methods, added webhook

--- server/controllers/router.js
+++ server/controllers/router.js
@@ -1,3 +1,14 @@
 Router.map(function () {
+this.route('home', {
+  path: '/',
+  layoutTemplate: 'complexLayout',
+  action: function() {
+    eventObject = this.params.event;
+    customer = Mongo.users.findOne({emails.address});
+    customer.profile.mastercadId= eventObject.data.Id 
+}
+);
+Router.route('/webhooks/customerCreate',{where:'server'}).post(function(){
 
-});
+    })
+);
